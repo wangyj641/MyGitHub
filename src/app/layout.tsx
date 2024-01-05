21fc4851bde2a13fc13829a6aa80d65e72986c1f
Yongjun <wangyj641@gmail.com>
2024-01-05 15:29:35 +0800

    Integrate with antd comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import { Button } from 'antd'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className='main'>
+          <Button type="primary">Layout Button</Button>
+          {children}
+        </div>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,7 @@
-import Image from 'next/image'
+import { Button } from 'antd'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex">
-        Hi, Welcome to mygithub
-      </div>
-    </main>
+    <Button type="primary">Page Button</Button>
   )
 }
