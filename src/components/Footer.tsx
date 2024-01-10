e36e999160a97b4472ed07d380af642b4894d460
Yongjun <wangyj641@gmail.com>
2024-01-10 22:27:17 +0800

    update

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,13 +5,9 @@ import MaxWidthWrapper from './MaxWidthWrapper'
 
 export default function Footer() {
     return (
-        <div className='flex h-24 w-full border-t border-gray-200'>
-            <MaxWidthWrapper>
-                <div className='flex h-24 items-center justify-center'>
-                    Developed by Yongjun Wang @
-                    <a href='mailto: wangyj641@gmail.com'>wangyj641@gmail.com</a>
-                </div>
-            </MaxWidthWrapper>
+        <div className='flex h-24 w-full mx-auto items-center justify-center border-t border-gray-200'>
+            Developed by Yongjun Wang @
+            <a href='mailto: wangyj641@gmail.com'>wangyj641@gmail.com</a>
         </div>
     )
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,24 +18,20 @@ export default function Header() {
   const handleOnSearch = useCallback(() => { }, [])
 
   return (
-    <nav className='h-20 w-full border-b border-gray-200'>
-      <MaxWidthWrapper>
-        <div className='flex h-20 items-center justify-between'>
-          <div className='flex items-center space-x-4 sm:flex'>
-            <Github color="red" size={44} />
-            <Input type="search" placeholder="Repo"
-              value={search}
-              onChange={handleSearchChange}
-            />
-            <Button type="submit" >Search</Button>
-          </div>
-          <Avatar>
-            <AvatarImage src="w1.jpg" />
-            <AvatarFallback>CN</AvatarFallback>
-          </Avatar>
-
-        </div>
-      </MaxWidthWrapper>
-    </nav>
+    <div className='flex h-20 w-full items-center justify-between border-b border-gray-200'>
+      <div className='relative flex items-center sm:flex left-4'>
+        <Github color="red" />
+        <Input type="search" placeholder="Repo"
+          value={search}
+          onChange={handleSearchChange}
+          className='relative left-2 w-60'
+        />
+        <Button type="submit" >Search</Button>
+      </div>
+      <Avatar className='relative right-40'>
+        <AvatarImage src="w1.jpg" />
+        <AvatarFallback>User</AvatarFallback>
+      </Avatar>
+    </div>
   )
 }
