a2f2db1f9ea1a11a2b59cf3dd089ea3f798cb33a
Yongjun <wangyj641@gmail.com>
2024-01-18 08:46:13 +0800

    update

---
+++ src/pages/detail.tsx
@@ -0,0 +1,7 @@
+export default function detail() {
+    return (
+        <div>
+            detail
+        </div>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,26 +1,7 @@
-import React, { useCallback, useState, useEffect } from 'react'
-//import { Button } from "@/components/ui/button"
-import getConfig from 'next/config'
-import axios from 'axios'
-
-const { publicRuntimeConfig } = getConfig()
-
-const handleLogin = () => {
-  console.log('------ handleLogin ------')
-}
-
 export default function Index() {
-  useEffect(() => {
-    console.log('------ get user info ------')
-    axios.get('/api/user/info').then(res => {
-      console.log(res.data)
-    })
-  }, [])
-
   return (
     <div className="flex flex-col w-full h-full justify-between items-center p-1">
-      <span>Not login in </span>
-      <a href={publicRuntimeConfig.OAUTH_URL}>Login</a>
+      <span>Index</span>
     </div>
   )
 }
