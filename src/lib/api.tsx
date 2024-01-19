1c9e8afcabf97a4e3e9c10481d532dd3fc09fc77
Yongjun <wangyj641@gmail.com>
2024-01-19 17:08:21 +0800

    Fix bugs

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -27,7 +27,7 @@ async function request({ method = 'GET', url, data = {} }, req, res) {
     if (githubAuth.access_token) {
       headers['Authorization'] = `${githubAuth.token_type} ${githubAuth.access_token}`
     }
-    return await requestGithub({ method, url, data, headers })
+    return await requestGithub(method, url, data, headers)
   }
   else {
     return await axios({
