9451e3a127ced94ec08e94072b01877166310a90
Yongjun <wangyj641@gmail.com>
2024-01-21 23:09:05 +0800

    Call search API

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -1,8 +1,12 @@
 import { withRouter } from "next/router"
 
-function search({ router }) {
+const api = require("../lib/api")
+
+function search({ router, repos }) {
   console.log('---------------- search ----------------')
   //console.log(router)
+  console.log(repos)
+
   const query = router.query.q
   console.log(query)
 
@@ -11,6 +15,49 @@ function search({ router }) {
       <span>search</span>
     </div>
   )
+}
+
+search.getInitialProps = async ({ ctx }) => {
+  console.log('---------------- search getInitialProps ----------------')
+  console.log(ctx)
+  console.log(ctx.query)
+  const { q, sort, lang, order, page } = ctx.query
+
+  if (!q) {
+    return {
+      repos: {
+        total_count: 0,
+      }
+    }
+  }
+
+  console.log('---------------- search getInitialProps 2----------------')
+  let queryString = `?q=${q}`
+  if (lang) {
+    queryString += `&l=${lang}`
+  }
+
+  if (sort) {
+    queryString += `&sort=${sort}&order=${order || 'desc'}`
+  }
+
+  if (page) {
+    queryString += `&page=${page}`
+  }
+
+  console.log(queryString)
+
+  const result = await api.request(
+    {
+      url: `search/repositories${queryString}`
+    },
+    ctx.req,
+    ctx.res
+  )
+
+  return {
+    repos: result.data
+  }
 }
 
 export default withRouter(search)
