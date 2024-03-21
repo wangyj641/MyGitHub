dc563228e63c825a3e6853709289be4548cf16fe
Yongjun <wangyj641@gmail.com>
2024-03-21 22:18:07 +0800

    Upload screenshot image

--- global.config.js
+++ global.config.js
@@ -5,9 +5,9 @@ const SCOPE = 'user';
 
 // https://github.com/settings/developers
 // 在 github 上注册的 App 的 Client ID
-const client_id = 'f257cf698640cc032c19';
+const client_id = '246560ac02e68a37f12b';
 // 在 github 上注册的 App 的 Client Secret
-const client_secret = '75a16e73ea6913b8c84ebb1427988d338e5e3e08';
+const client_secret = 'a41f9423c793d224b3d268efb1d26e28e9faa281';
 
 module.exports = {
   github: {
--- src/server/auth.tsx
+++ src/server/auth.tsx
@@ -4,82 +4,82 @@ const config = require('../../global.config.js')
 const { client_id, client_secret, request_token_url } = config.github
 
 module.exports = (server) => {
-    server.use(async (ctx, next) => {
-        if (ctx.path === '/auth') {
-            console.log('------ auth ------')
-            const code = ctx.query.code
-            if (!code) {
-                ctx.body = {
-                    code: 400,
-                    data: 'code is required'
-                }
-                return
-            } else {
-                console.log(code)
-                const res = await axios({
-                    method: 'post',
-                    url: request_token_url,
-                    data: {
-                        client_id,
-                        client_secret,
-                        code
-                    },
-                    headers: {
-                        'Accept': 'application/json'
-                    }
-                })
-
-                console.log(res.status, res.data)
-
-                if ((res.status === 200) && (res.data && !res.data.error)) {
-                    console.log('------ get token success ------')
-                    ctx.session.githubAuth = res.data
-                    const { access_token, token_type } = res.data
-
-                    const userInfoResp = await axios({
-                        method: 'GET',
-                        url: 'https://api.github.com/user',
-                        headers: {
-                            'Authorization': `${token_type} ${access_token}`,
-                        },
-                    })
-
-                    console.log(userInfoResp.status, userInfoResp.data)
-                    ctx.session.userInfo = userInfoResp.data
-                    ctx.redirect((ctx.session && ctx.session.urlBeforeOAuth) || '/')
-                    ctx.session.urlBeforeOAuth = null
-                } else {
-                    const errorMsg = res.data && res.data.error
-                    ctx.body = {
-                        code: 400,
-                        data: `request token failed ${errorMsg}`
-                    }
-                }
-                return
-            }
-        } else {
-            await next()
+  server.use(async (ctx, next) => {
+    if (ctx.path === '/auth') {
+      console.log('------ auth ------')
+      const code = ctx.query.code
+      if (!code) {
+        ctx.body = {
+          code: 400,
+          data: 'code is required'
         }
-    })
+        return
+      } else {
+        console.log(code)
+        const res = await axios({
+          method: 'post',
+          url: request_token_url,
+          data: {
+            client_id,
+            client_secret,
+            code
+          },
+          headers: {
+            'Accept': 'application/json'
+          }
+        })
 
-    server.use(async (ctx, next) => {
-        if (ctx.path === '/logout' && ctx.method === 'POST') {
-            console.log('------ auth logout ------')
-            ctx.session = null
-            ctx.body = 'logout success'
-        } else {
-            await next()
-        }
-    })
+        console.log(res.status, res.data)
 
-    server.use(async (ctx, next) => {
-        if (ctx.path === '/prepare-auth' && ctx.method === 'GET') {
-            console.log('------ auth prepare-auth ------')
-            const { url } = ctx.query
-            ctx.session.urlBeforeOAuth = url
-            ctx.redirect(config.OAUTH_URL)
+        if ((res.status === 200) && (res.data && !res.data.error)) {
+          console.log('------ get token success ------')
+          ctx.session.githubAuth = res.data
+          const { access_token, token_type } = res.data
+
+          const userInfoResp = await axios({
+            method: 'GET',
+            url: 'https://api.github.com/user',
+            headers: {
+              'Authorization': `${token_type} ${access_token}`,
+            },
+          })
+
+          console.log(userInfoResp.status, userInfoResp.data)
+          ctx.session.userInfo = userInfoResp.data
+          ctx.redirect((ctx.session && ctx.session.urlBeforeOAuth) || '/')
+          ctx.session.urlBeforeOAuth = null
         } else {
-            await next()
+          const errorMsg = res.data && res.data.error
+          ctx.body = {
+            code: 400,
+            data: `request token failed ${errorMsg}`
+          }
         }
-    })
+        return
+      }
+    } else {
+      await next()
+    }
+  })
+
+  server.use(async (ctx, next) => {
+    if (ctx.path === '/logout' && ctx.method === 'POST') {
+      console.log('------ auth logout ------')
+      ctx.session = null
+      ctx.body = 'logout success'
+    } else {
+      await next()
+    }
+  })
+
+  server.use(async (ctx, next) => {
+    if (ctx.path === '/prepare-auth' && ctx.method === 'GET') {
+      console.log('------ auth prepare-auth ------')
+      const { url } = ctx.query
+      ctx.session.urlBeforeOAuth = url
+      ctx.redirect(config.OAUTH_URL)
+    } else {
+      await next()
+    }
+  })
 }
